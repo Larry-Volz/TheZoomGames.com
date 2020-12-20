ec5f3cfa49f0a99d3926bf8321b228f77a6214e4
NovusLio <hayatonao@outlook.com>
2020-12-21 01:18:35 +0800

    index.php
    Signed-off-by: NovusLio <hayatonao@outlook.com>

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
-error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR);
-define('APP_NAME', 'awesome-zoom');
+$phpversion = '5.4.0';
+if(version_compare(PHP_VERSION,$phpversion,'<')) die("require PHP > $phpversion !");
 define('DSE', DIRECTORY_SEPARATOR);
-define('APP_ROOT', __DIR__.DSE);
-define('APP_CORE', APP_ROOT.'awesome-zoom'.DSE);
-define('APP_BOOTSTRAP', APP_CORE.'bootstrap'.DSE);
+define('APP_DEBUG',true);
+$dse = DSE;
 
-if (!is_file(APP_BOOTSTRAP.'app.php'))
-    die('Cannot find the `bootstrap/app.php` file!');
-require APP_BOOTSTRAP.'app.php';
+if (isset($_REQUEST['FIRST_FOLDER']) && $_REQUEST['FIRST_FOLDER'])
+    $folder = $_REQUEST['FIRST_FOLDER'];
+else
+    $folder = "zoom";
+
+$path = ".${dse}${folder}${dse}";
+define('APP_PATH',$path);
+define('APP_NAME','app_name');
+define('_PHP_FILE_',$_SERVER['SCRIPT_NAME']);
+
+require 'zoom/ThinkPHP/ThinkPHP.php';
